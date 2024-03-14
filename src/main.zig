a338f457d01e4db8c8fe5e4b726913339b9ef441
judehahh <liujh2818@outlook.com>
2024-03-14 13:36:31 +0800

    Support plus, minus and number_literal

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,71 @@
 const std = @import("std");
+const Tokenizer = @import("Tokenizer.zig");
 
 pub fn main() !void {
-    const argv = std.os.argv;
-    if (argv.len != 2) {
-        const err = std.io.getStdErr().writer();
-        try err.print("{s}: invalid number of arguments\n", .{argv[0]});
-        std.os.exit(1);
-    }
-    const number = try std.fmt.parseInt(i64, std.mem.span(argv[1]), 10);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
+
+    if (std.os.argv.len != 2) @panic("Usage: jcc file");
+
+    const file = std.fs.cwd().openFile(std.mem.span(std.os.argv[1]), .{ .mode = .read_only }) catch {
+        std.debug.panic("Can not open {s}\n", .{std.os.argv[1]});
+    };
+    defer file.close();
+
+    // Use readToEndAllocOptions() to add an sentinel "0" at the end of the source.
+    const source = try file.readToEndAllocOptions(allocator, std.math.maxInt(u32), null, @alignOf(u8), 0);
+    defer allocator.free(source);
 
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
+    var tokenizer = Tokenizer.init(source);
+
     try stdout.print("  .globl main\n", .{});
     try stdout.print("main:\n", .{});
-    try stdout.print("  li a0, {d}\n", .{number});
-    try stdout.print("  ret\n", .{});
 
+    while (true) {
+        const tok = tokenizer.next();
+        switch (tok.tag) {
+            .number_literal => try stdout.print(
+                "  li a0, {d}\n",
+                .{try std.fmt.parseInt(
+                    u32,
+                    source[tok.loc.start..tok.loc.end],
+                    10,
+                )},
+            ),
+            .plus => {
+                const tok_next = tokenizer.next();
+                if (tok_next.tag != .number_literal) @panic("Parse error");
+                try stdout.print(
+                    "  addi a0, a0, {d}\n",
+                    .{try std.fmt.parseInt(
+                        u32,
+                        source[tok_next.loc.start..tok_next.loc.end],
+                        10,
+                    )},
+                );
+            },
+            .minus => {
+                const tok_next = tokenizer.next();
+                if (tok_next.tag != .number_literal) @panic("Parse error");
+                try stdout.print(
+                    "  addi a0, a0, -{d}\n",
+                    .{try std.fmt.parseInt(
+                        u32,
+                        source[tok_next.loc.start..tok_next.loc.end],
+                        10,
+                    )},
+                );
+            },
+            .eof => break,
+            else => @panic("TODO: parse more tags"),
+        }
+    }
+
+    try stdout.print("  ret\n", .{});
     try bw.flush();
 }
